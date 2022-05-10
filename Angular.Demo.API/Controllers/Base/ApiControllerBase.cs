3ff69f40513b6c02870c4b6f05d410e2d447cf46
Bernth, Claus <cbernth@advent.com>
2022-05-10 11:49:24 +0200

    Adding ApiControllerBase to define shared properties of the API

---
+++ Angular.Demo.API/Controllers/Base/ApiControllerBase.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Angular.Demo.API.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]/[action]")]
+	[Produces("application/json")]
+	public class ApiControllerBase : ControllerBase
+	{ }
+
+}

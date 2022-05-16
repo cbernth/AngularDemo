405339a13275e4c5e93c1777f1d547ed77b14a61
Bernth, Claus <cbernth@advent.com>
2022-05-16 13:57:14 +0200

    Removed uncessary ViewEncapsulation

--- Angular.Demo.UI/src/app/app.component.ts
+++ Angular.Demo.UI/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { filter, map } from 'rxjs';
@@ -8,8 +8,7 @@ import { AppConfigService } from './app-config.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
 
